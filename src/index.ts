095a3e616301a4ec212a706a1da35a65cba500d1
yankit <yankit.alone@gmail.com>
2022-11-12 21:25:09 +0800

    完成获取当前月份上个月以及下个月月份的补充

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,7 @@ export default class Calendar {
   constructor() {
     this.date = new Date()
     this.currentYear = this.date.getFullYear()
+    // this.currentMonth = this.date.getMonth() + 1
     this.currentMonth = this.date.getMonth() + 1
     this.currentDay = new Date(this.date).getDate()
     const week = new Date(this.date).getDay()
@@ -16,7 +17,6 @@ export default class Calendar {
     } else {
       this.currentWeek = week
     }
-    console.log(this.currentWeek)
     this.getCurrentMonthCalendar(this.currentYear, this.currentMonth)
   }
 
@@ -31,20 +31,15 @@ export default class Calendar {
     return days
   }
 
-  // FIXME: 在new Date()的时候，month哪怕是正确的 但是在底层处理的时候，会按照 0 - 11来处理月份
+  // 在new Date()的时候，month哪怕是正确的 但是在底层处理的时候，会按照 0 - 11来处理月份
   public getCurrentMonthCalendar(year: number, month: number): Date[] {
+    const simpleCalendar: Array<number[]> = []
     // 先获取当前月的上一个月的和下一个月的月头以及月尾
     const preYear = year - 1
     const afterYear = year + 1
     const preMonth = month - 1 === 0 ? 12 : month - 1
     const afterMonth = month + 1 === 13 ? 1 : month + 1
-    console.log({
-      preYear,
-      preMonth,
-      afterYear,
-      afterMonth,
-    })
-    console.log(this.getDays(year, month))
+    console.log('pre after :>>', { preYear, preMonth, afterYear, afterMonth })
     // 获取当前月的一号是星期几
     // week range [0 - 6]
     const currentMonthOneDateWeek = new Date(year, month - 1, 1).getDay()
@@ -54,7 +49,37 @@ export default class Calendar {
       this.getDays(year, month)
     ).getDay()
 
-    console.log(currentMonthOneDateWeek, currentMonthLastDayWeek, year, month)
+    console.log('week :>>', currentMonthOneDateWeek, currentMonthLastDayWeek)
+    // get preMonth days
+    const preMonthDays = this.getDays(
+      preMonth === 12 ? preYear : year,
+      preMonth
+    )
+    const afterMonthDays = this.getDays(
+      afterMonth === 1 ? afterYear : year,
+      afterMonth
+    )
+
+    // 获取前面缺失的几天
+    for (let i = currentMonthOneDateWeek - 1; i >= 0; i--) {
+      console.log(i)
+      simpleCalendar.push([
+        preMonth === 12 ? preYear : year,
+        preMonth,
+        preMonthDays - i,
+      ])
+    }
+
+    simpleCalendar.push([])
+
+    // 获取后面缺失的几天
+    for (let i = 1; i <= currentMonthLastDayWeek; i++) {
+      simpleCalendar.push([afterMonth === 1 ? afterYear : year, afterMonth, i])
+    }
+
+    console.log(simpleCalendar)
+
+    console.log(preMonthDays, afterMonthDays)
     return []
   }
 }
