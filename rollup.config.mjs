be2f6f2af939bbd9f25db1ae942e969f0b945305
yankit <yankit.alone@gmail.com>
2022-11-13 02:10:10 +0800

    更新打包配置；更新packge.json信息

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -3,9 +3,15 @@ import typescript from 'rollup-plugin-typescript2'
 
 export default {
   input: path.resolve(__dirname, './src/index.ts'),
-  output: {
-    file: path.resolve(__dirname, './dist/index.js'),
-    format: 'es',
-  },
+  output: [
+    {
+      file: path.resolve(__dirname, './dist/index.esm.js'),
+      format: 'es',
+    },
+    {
+      file: path.resolve(__dirname, './dist/index.com.js'),
+      format: 'cjs',
+    },
+  ],
   plugins: [typescript()],
 }
